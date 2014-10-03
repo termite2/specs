65d6b33052a76d3597211a618fb7b79af68b7b89
Leonid Ryzhyk <leonidr@cse.unsw.edu.au>
2014-10-02 21:37:38 -0400

    termite.h declares standard macros used in generated C code

---
+++ include/termite.h
@@ -0,0 +1,16 @@
+/* Common data types used in Termite drivers.  
+ * TODO: replace with OS-specific includes. */
+
+#ifndef _TERMITE_H
+#define _TERMITE_H
+
+typedef unsigned char      u8;
+typedef char               s8;
+typedef unsigned short     u16;
+typedef short              s16;
+typedef unsigned int       u32;
+typedef int                s32;
+typedef unsigned long long u64;
+typedef long long          s64;
+
+#endif /* _TERMITE_H */
